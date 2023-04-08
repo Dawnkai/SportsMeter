6c61089544d8915c50245f33ccb1a7fd623d47be
Maciej Kleban <dawnwender@gmail.com>
2023-04-08 14:40:32 +0200

    Add Main Page to index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-import { useState, useEffect, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Example from './Example';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import './styles.css';
+
+const theme = createTheme();
+
+const Copyright = () => {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
 
 const App = () => {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        fetch('/api')
-        .then((res) => res.json())
-        .then((data) => {
-            setData(data['response']);
-        });
-    }, []);
-
-    return <Example value={data} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<MainPage/>}/>
+                    <Route path="/login" element={<SignInPage/>}/>
+                    <Route path="/register" element={<SignUpPage/>}/>
+                </Routes>
+            </BrowserRouter>
+            <Copyright/>
+        </ThemeProvider>
+    );
 }
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <StrictMode>
-        <App/>
-    </StrictMode>
+    <App/>
 )
