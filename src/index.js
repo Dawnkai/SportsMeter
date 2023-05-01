92a61d46014baaf2af9b5f6fc5add7cb2577aba8
Maciej Kleban <dawnwender@gmail.com>
2023-05-01 14:47:19 +0200

    Update copyright panel
    Add authors and website name

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ const theme = createTheme({
 const Copyright = () => {
     return (
         <Typography variant="body2" color="text.secondary" align="center">
-            {'Copyright © '}
+            {'Copyright Maciej Kleban, Krzysztof Herbot © '}
             <Link color="inherit" href="https://mui.com/">
-                Your Website
+                SportsMeter
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
