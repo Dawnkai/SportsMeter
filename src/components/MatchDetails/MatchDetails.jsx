c78d1f62730db41817f46623b29234c43a1fd021
Maciej Kleban <dawnwender@gmail.com>
2023-05-01 14:58:54 +0200

    Adapt match details to new changes
    * Disable match editing
    * Display all match data from backend
    * Convert time and date to proper format

--- src/components/MatchDetails/MatchDetails.jsx
+++ src/components/MatchDetails/MatchDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -9,16 +10,23 @@ import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 
-import MatchForm from './MatchForm';
-import { Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-import { useNavigate } from 'react-router-dom';
+import MatchForm from './MatchForm';
+
+import parseDate from '../../utils/parseDate';
+import parseTime from '../../utils/parseTime';
 
 export default function MatchDetails() {
     const { match_id } = useParams();
     const [ isEditing, setIsEditing ] = useState(false);
-    const [ matchDetails, setMatchDetails ] = useState({"match_id": 0, "match_title": "", "season_title": ""});
+    const [ matchDetails, setMatchDetails ] = useState(
+        {
+            "match_id": 0, "team_a_name": "", "team_a_points": 0,
+            "team_b_name": "", "team_b_points": 0, "match_date": "",
+            "match_start_time": "", "match_end_time": ""
+        }
+    );
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -49,6 +57,18 @@ export default function MatchDetails() {
             console.log(error);
             navigate("/");
         });
+    }
+
+    const DateText = (dateStr) => {
+        const date = parseDate(dateStr, false);
+        if (date === null) return "";
+        return `${date["day"]}.${date["month"]}.${date['year']}`;
+    }
+
+    const TimeText = (timeStr) => {
+        const time = parseTime(timeStr, false);
+        if (time === null) return "";
+        return `${time["hours"]}:${time["minutes"]}:${time["seconds"]}`;
     }
 
     return (
@@ -98,19 +118,19 @@ export default function MatchDetails() {
                                 <ListItem>
                                     <ListItemText
                                         primary="Match date"
-                                        secondary={matchDetails?.match_date}
+                                        secondary={DateText(matchDetails?.match_date)}
                                     />
                                 </ListItem>
                                 <ListItem>
                                     <ListItemText
                                         primary="Match start time"
-                                        secondary={matchDetails?.match_start_time}
+                                        secondary={TimeText(matchDetails?.match_start_time)}
                                     />
                                 </ListItem>
                                 <ListItem>
                                     <ListItemText
                                         primary="Match end time"
-                                        secondary={matchDetails?.match_end_time}
+                                        secondary={TimeText(matchDetails?.match_end_time)}
                                     />
                                 </ListItem>
                             </List>
